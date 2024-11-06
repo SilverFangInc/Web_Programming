ab78d62434656fb95ae6c3f1cd6a8adfd46554a6
scott16-afk <sdpirk3013@ung.edu>
2024-11-06 18:48:35 -0500

    Update Leadership_code.js

--- Work/Working_On_IT/Leadership_code.js
+++ Work/Working_On_IT/Leadership_code.js
@@ -1,16 +1,16 @@
-setTimeout(changing_Stuffs, 3000);
-
-//changes images and text to show off different pictures
-function changing_Stuffs(imgNum) {
-   if (imgNum == 0) {
-        let img = document.getElementById("dog_img");
-        img.src = "https://images.pexels.com/photos/6209458/pexels-photo-6209458.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1";
-        let p = document.getElementById("dog_name");
-        p.innerHTML = "Bigger Christmas Car";
-     } else if (imgNum == -1) {
-        let img = document.getElementById("dog_img");
-        img.src = "https://images.pexels.com/photos/6119572/pexels-photo-6119572.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1";
-        let p = document.getElementById("dog_name");
-        p.innerHTML = "Tree and Chair";
+let slideIndex = 0;
+  showSlides();
+  
+  function showSlides() {
+    let i;
+    let slides = document.getElementsByClassName("Dog_img");
+  
+    // Hide all slides
+    for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";  
     }
-}
+    slideIndex++; //increment the slideIndex 
+    if (slideIndex > slides.length) {slideIndex = 1}    
+    slides[slideIndex-1].style.display = "block";  //show the current slide
+    setTimeout(showSlides, 1000); // Change image and dot every 4 seconds
+  }
